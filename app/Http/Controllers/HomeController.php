a246d51a5533d22d70e820a673f1e6855601bfd8
cbbomhay2000 <canhdong1999@gmail.com>
2022-04-19 16:31:53 +0700

    fix changepassword

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,62 +1,37 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Requests\ChangePasswordRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth');
     }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         return view('home');
     }
     public function reset()
     {
-        return view('auth/passwords/resetpassword');
+        return view('auth.passwords.resetpassword');
     }
 
-     /**
-     * Change the current password
-     * @param Request $request
-     * @return Renderable
-     */
-    public function changePassword(Request $request)
-    {       
-        $user = Auth::user();
-    
-        $userPassword = $user->password;
-        
-        $request->validate([
-            'current_password' => 'required',
-            'password' => 'required|same:confirm_password|min:6',
-            'confirm_password' => 'required',
-        ]);
+    public function changePassword(ChangePasswordRequest $request)
+    {
+        $user = User::find(Auth::user()->id);
 
-        if (!Hash::check($request->current_password, $userPassword)) {
-            return back()->withErrors(['current_password'=>'password not match']);
+        if (!Hash::check($request->current_password, $user->password)) {
+            return redirect()->back()->with('fail', 'Da say ra loi');
         }
 
-        $user->password = Hash::make($request->password);
-
-        $user->save();
-
-        return redirect()->back()->with('success','password successfully updated');
+        $user->update([
+            'password' => Hash::make($request->password),
+        ]);
+        return redirect()->back()->with('success', 'password successfully updated');
     }
 }
