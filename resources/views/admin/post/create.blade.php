a0e896126f08b5cff14b3234bee5956659c7839d
cbbomhay2000 <canhdong1999@gmail.com>
2022-05-04 01:27:46 +0700

    edit

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -2,9 +2,7 @@
 <head>
     <meta charset="utf-8">
     <title>Viết bài</title>
-    <link rel="stylesheet" type="text/css" id="style-ref"
-        href="https://hashnode.com/static/css/app.min.css?v=1625504643155">
-    <link href="{{ url('public/site') }}/css/sb-admin-2.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" id="style-ref"href="https://hashnode.com/static/css/app.min.css?v=1625504643155">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
@@ -68,61 +66,11 @@
         </div>
     </div>
 </body>
-<script src="{{ url('public/site') }}/vendor/jquery/jquery.min.js"></script>
-<script src="{{ url('public/site') }}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="{{ url('public/site') }}/vendor/jquery-easing/jquery.easing.min.js"></script>
 <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
-<script src="{{ url('public/site') }}/js/sb-admin-2.min.js"></script>
 <script>
     $(document).ready(function() {
-
-        var lfm = function(options, cb) {
-            var route_prefix = (options && options.prefix) ? options.prefix : '/laravel-filemanager';
-            window.open(route_prefix + '?type=' + options.type || 'file', 'FileManager',
-                'width=900,height=600');
-            window.SetUrl = cb;
-        };
-
-        var LFMButton = function(context) {
-            var ui = $.summernote.ui;
-            var button = ui.button({
-                contents: '<i class="note-icon-picture"></i> ',
-                tooltip: 'Insert image with filemanager',
-                click: function() {
-
-                    lfm({
-                        type: 'image',
-                        prefix: '/lrv8x_blog/laravel-filemanager'
-                    }, function(lfmItems, path) {
-                        lfmItems.forEach(function(lfmItem) {
-                            context.invoke('insertImage', lfmItem.url);
-                        });
-                    });
-
-                }
-            });
-            return button.render();
-        };
-
         $('#summernote').summernote({
-            toolbar: [
-                ['popovers',
-                    ['lfm']
-                ],
-                ['style', ['bold', 'italic', 'underline', 'clear']],
-                ['font', ['strikethrough', 'superscript', 'subscript']],
-                ['fontsize', ['fontsize']],
-                ['color', ['color']],
-                ['para', ['ul', 'ol', 'paragraph']],
-                ['height', ['height']],
-                ['link'],
-                ['codeview'],
-            ],
-
-            buttons: {
-                lfm: LFMButton
-            },
             height: 300,
             placeholder: "Chi tiết bài viết của bạn",
             minHeight: null,
